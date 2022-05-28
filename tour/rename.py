748b7e6329b18317a7fab8875d5eb58c4b68b3e9
Derek Wan <d.wan@icloud.com>
2022-05-28 22:27:17 +0800

    More

--- tour/rename.py
+++ tour/rename.py
@@ -1,12 +1,15 @@
 from pathlib import Path
-from contextlib import suppress
 
 
 for path in sorted(Path("tour").iterdir()):
-    with suppress(ValueError):
-        i, name = path.name.split("_", 1)
-        i = int(i)
-        new_name = f"{i:02}_{name}"
-        new_path = path.with_name(new_name)
-        print(path, name, new_name, new_path)
-        # path.rename(new_path)
+    if path.suffix == ".go":
+        try:
+            i, name = path.name.split("_", 1)
+        except ValueError:
+            print(f"Skipping {path}")
+        else:
+            i = int(i)
+            new_name = f"{i:02}_{name}"
+            new_path = path.with_name(new_name)
+            print(f"Renaming\n  {path}\n> {new_path}")
+            # path.rename(new_path)
