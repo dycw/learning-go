9604d4c677fe2a3c300d3c6055558ae57fec7bba
Derek Wan <d.wan@icloud.com>
2022-05-29 17:25:01 +0800

    Closure

--- tour/4-more-types/26-exercise-fibonacci-closure.go
+++ tour/4-more-types/26-exercise-fibonacci-closure.go
@@ -5,19 +5,18 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	i, j := 1, 1
+	i, j := 0, 1
+	return func() int {
+		sum := i + j
+		i, j = j, sum
+		return sum
+
+	}
 }
 
 func main() {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
 		fmt.Println(f())
-	}
-}
-
-func main() {
-	pos, neg := adder(), adder()
-	for i := 0; i < 10; i++ {
-		fmt.Println(pos(i), neg(-2*i))
 	}
 }
