400117695e462557086ca07787776d59762fb715
Derek Wan <d.wan@icloud.com>
2022-05-29 17:13:16 +0800

    WIP

--- tour/rename.py
+++ tour/rename.py
@@ -11,15 +11,29 @@ def main() -> None:
 
     # main
     paths = sorted(Path.cwd().glob("**/*.go"))
+    paths = (path for path in paths if is_process(path))
     for _, path in enumerate(paths, start=1):
-        new_path = func(path)
-        print(f"Renaming\n  {path}\n> {new_path}")
-        if do:
-            path.rename(new_path)
+        func(path, do=do)
 
 
-def func(path: Path) -> Path:
-    return path.with_name(path.name.replace("_", "-"))
+def is_process(path: Path) -> bool:
+    return (
+        path.name != "main.go"
+        and path.suffix == ".go"
+        and path.parent.is_dir()
+        and len(list(path.parent.iterdir())) > 1
+    )
+
+
+def func(path: Path, *, do: bool) -> None:
+    i, name = path.with_suffix("").name.split("-", 1)
+    i = int(i)
+    new_path = path.parent.joinpath(f"{i}-{name}", "main.go")
+    print(f"Propose\n    {path}\n--> {new_path}")
+
+    if do:
+        path.rename(new_path)
+        print("...done")
 
 
 if __name__ == "__main__":
