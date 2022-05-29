1f3420ccf9a555388e877ef936db02472081f591
Derek Wan <d.wan@icloud.com>
2022-05-29 20:27:52 +0800

    group

--- tour/5-methods/22-exercise-reader/main.go
+++ tour/5-methods/22-exercise-reader/main.go
@@ -5,10 +5,9 @@ import "golang.org/x/tour/reader"
 type MyReader struct{}
 
 // TODO: Add a Read([]byte) (int, error) method to MyReader.
-func (reader MyReader) Read(b []byte) (int, error) {
-	a := "A"
+func (r MyReader) Read(b []byte) (int, error) {
 	for i := 0; i < len(b); i++ {
-		b[i] = a
+		b[i] = 'A'
 	}
 	return len(b), nil
 }
