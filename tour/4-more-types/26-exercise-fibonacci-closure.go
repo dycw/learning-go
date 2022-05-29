b303abcf1e4cfe50f4e737c936601bc31f393457
Derek Wan <d.wan@icloud.com>
2022-05-29 17:21:51 +0800

    Remove fuzz data

--- tour/4-more-types/26-exercise-fibonacci-closure.go
+++ tour/4-more-types/26-exercise-fibonacci-closure.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
-func adder() func(int) int {
-	sum := 0
-	return func(x int) int {
-		sum += x
-		return sum
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	i, j := 1, 1
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
 	}
 }
 
